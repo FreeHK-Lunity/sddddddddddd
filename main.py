11bb4abb2b301785a6d0d21f5013f75aa038c3ac
ed <67418738+FreeHK-Lunity@users.noreply.github.com>
2023-05-03 10:35:00 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ def Turn_left():
     CUHK_JC_iCar.car_ctrl_speed(CUHK_JC_iCar.CarState.SPIN_LEFT, 10)
 def move_forward():
     CUHK_JC_iCar.car_ctrl_speed(CUHK_JC_iCar.CarState.FORWARD, 80)
+xeee = 0
+husk_appear3 = False
+husk_appear2 = False
+husk_appear = False
 huskylens.init_i2c()
-huskylens.init_mode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
-
+huskylens.init_mode(protocolAlgorithm.OBJECTCLASSIFICATION)
 
 def on_forever():
+    global husk_appear, husk_appear2, husk_appear3, xeee
     husk_appear = huskylens.is_appear(1, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
     husk_appear2 = huskylens.is_appear(2, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
     husk_appear3 = huskylens.is_appear(3, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
@@ -26,7 +30,9 @@ def on_forever():
     if xeee == 2:
         Turn_right()
         move_forward()
+        basic.show_icon(IconNames.HEART)
     if xeee == 3:
         Turn_left()
         move_forward()
+        basic.show_icon(IconNames.HEART)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
-function Turn_right () {
+function Turn_right() {
     CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.SpinRight, 10)
 }
-function Turn_left () {
+
+function Turn_left() {
     CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.SpinLeft, 10)
 }
-function move_forward () {
+
+function move_forward() {
     CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.Forward, 80)
 }
+
 let xeee = 0
 let husk_appear3 = false
 let husk_appear2 = false
 let husk_appear = false
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.OBJECTCLASSIFICATION)
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     husk_appear = huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)
     husk_appear2 = huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)
     husk_appear3 = huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)
@@ -25,17 +29,23 @@ basic.forever(function () {
     } else if (husk_appear3 == true && husk_appear == false) {
         xeee = 3
     } else {
-    	
+        
     }
+    
     if (xeee == 1) {
         basic.showIcon(IconNames.Scissors)
     }
+    
     if (xeee == 2) {
         Turn_right()
         move_forward()
+        basic.showIcon(IconNames.Heart)
     }
+    
     if (xeee == 3) {
         Turn_left()
         move_forward()
+        basic.showIcon(IconNames.Heart)
     }
+    
 })
