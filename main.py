ae1e89ff7fc23c86863ec95a44fc7d330001dcb0
ed <67418738+FreeHK-Lunity@users.noreply.github.com>
2023-05-03 10:26:33 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ def move_forward():
     CUHK_JC_iCar.car_ctrl_speed(CUHK_JC_iCar.CarState.FORWARD, 80)
 huskylens.init_i2c()
 huskylens.init_mode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
-husk_appear = huskylens.is_appear(1, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
-husk_appear2 = huskylens.is_appear(2, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
-husk_appear3 = huskylens.is_appear(3, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
-xeee = 999
-if husk_appear == True and husk_appear2 == False or husk_appear3 == False:
-    xeee = 1
-elif husk_appear2 == True and husk_appear == False:
-    xeee = 2
-elif husk_appear3 == True and husk_appear == False:
-    xeee = 3
-else:
-    pass
-if xeee == 1:
-    basic.show_icon(IconNames.SCISSORS)
-if xeee == 2:
-    Turn_right()
-    move_forward()
-if xeee == 3:
-    Turn_left()
-    move_forward()
+
 
 def on_forever():
-    pass
+    husk_appear = huskylens.is_appear(1, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
+    husk_appear2 = huskylens.is_appear(2, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
+    husk_appear3 = huskylens.is_appear(3, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
+    xeee = 999
+    if husk_appear == True and husk_appear2 == False or husk_appear3 == False:
+        xeee = 1
+    elif husk_appear2 == True and husk_appear == False:
+        xeee = 2
+    elif husk_appear3 == True and husk_appear == False:
+        xeee = 3
+    else:
+        pass
+    if xeee == 1:
+        basic.show_icon(IconNames.SCISSORS)
+    if xeee == 2:
+        Turn_right()
+        move_forward()
+    if xeee == 3:
+        Turn_left()
+        move_forward()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -7,32 +7,35 @@ function Turn_left () {
 function move_forward () {
     CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.Forward, 80)
 }
+let xeee = 0
+let husk_appear3 = false
+let husk_appear2 = false
+let husk_appear = false
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
-let husk_appear = huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)
-let husk_appear2 = huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)
-let husk_appear3 = huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)
-let xeee = 999
-if (husk_appear == true && husk_appear2 == false || husk_appear3 == false) {
-    xeee = 1
-} else if (husk_appear2 == true && husk_appear == false) {
-    xeee = 2
-} else if (husk_appear3 == true && husk_appear == false) {
-    xeee = 3
-} else {
-	
-}
-if (xeee == 1) {
-    basic.showIcon(IconNames.Scissors)
-}
-if (xeee == 2) {
-    Turn_right()
-    move_forward()
-}
-if (xeee == 3) {
-    Turn_left()
-    move_forward()
-}
 basic.forever(function () {
-	
+    husk_appear = huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)
+    husk_appear2 = huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)
+    husk_appear3 = huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)
+    xeee = 999
+    if (husk_appear == true && husk_appear2 == false || husk_appear3 == false) {
+        xeee = 1
+    } else if (husk_appear2 == true && husk_appear == false) {
+        xeee = 2
+    } else if (husk_appear3 == true && husk_appear == false) {
+        xeee = 3
+    } else {
+    	
+    }
+    if (xeee == 1) {
+        basic.showIcon(IconNames.Scissors)
+    }
+    if (xeee == 2) {
+        Turn_right()
+        move_forward()
+    }
+    if (xeee == 3) {
+        Turn_left()
+        move_forward()
+    }
 })
